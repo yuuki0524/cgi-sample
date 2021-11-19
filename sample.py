00c47c2e3e521e8ebd7bd29c26abaec6f8b1446e
yuuki0524 <94689807+yuuki0524@users.noreply.github.com>
2021-11-19 22:25:23 +0900

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,12 @@
+import cgi
+import cgitb
+cgitb.enable()
+
+print("Content-Type: text/html")
+print()   
+
+form = cgi.FieldStorage()
+
+for key in form:
+    value = form[key].value
+    print('<p>%s: %s</p>' % (key, value))
